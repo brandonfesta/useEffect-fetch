b8795a86ee86fed97b11a805a7b8dd2fe0e6f68f
bra <brandon-f@Libero.it>
2026-01-11 19:32:16 +0100

    finito esperimento

--- fetch/src/App.jsx
+++ fetch/src/App.jsx
@@ -1,9 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  useEffect(() => {
+    async function fetchPosts(){
+      setLoading(true)
+      setError(false)
+      try{
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        let data = await response.json()
+        setPosts(data)
+      } catch{
+        setError(true)
+      } finally{
+        setLoading(false)
+      }
+    }
+    fetchPosts()
+  }, [])
   return (
     <div>
-      
+      <h1>Post:</h1>
+      {loading && <p>loading...</p>}
+      {error && <p>Errore!</p>}
+      {posts.length > 0 ?(
+        <ul>
+          {posts.map((post) => (<li>{post.title}</li>))}
+        </ul>) : (
+          <p>Non ci sono post per questo utente</p>
+        )}
     </div>
   )
 }
